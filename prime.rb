989779fed42b7f5e05aa5c5237ca8982a364853b
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-25 03:41:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number) 
+  number = (1..52).to_a 
+  number.map |choice|
+  if choice % 2 
+    if choice % 3 
+      if choice % 5 
+        if choice % 11 
+  else puts "prime numbers are {number} 
+      end 
+    end 
+  end 
+end 
+end 
+end 
