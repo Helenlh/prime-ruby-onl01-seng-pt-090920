e45fee45e5cf237818c8284ddecf0029e24630ea
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-05 01:37:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,13 @@
 # Add  code here!
 def prime?(number) 
   number = (1..52).to_a 
-  number.map |choice|
+  number.map {|choice|
   if choice % 2 
-    if choice % 3 
-      if choice % 5 
-        if choice % 11 
-  else puts "prime numbers are {number} 
+    elsif choice % 3 
+      elsif choice % 5 
+        elsif choice % 11 
+        elsif choice < 1
+  else puts "prime numbers are {number}"
+        end 
       end 
-    end 
-  end 
-end 
-end 
-end 
+
